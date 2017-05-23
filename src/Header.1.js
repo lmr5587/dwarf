4f7c48d8d47b3c43f9334d603fb01aefe96aef40
Lizette Romero <lmromero@anderson.edu>
2017-05-22 21:19:46 -0400

    make article text component

---
+++ src/Header.1.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import './Header.css'
+import HeadNavBar from'./HeadNavBar'
+class Header extends Component{
+    render(){
+        return (
+            <div className="expanded row header">
+            <div className="clearfix">
+                <div className="float-left logo">
+                <img src="http://www.iconninja.com/files/409/4/1006/axe-icon.svg" alt="axe" />
+                </div>
+                <HeadNavBar/>
+            </div>
+            <div className="headline text-center small-12 columns">
+                <h1>The Dwarf Underground</h1>
+                <h3>Delving deep for the <span className="gold">shiniest</span> news</h3>
+            </div>
+            </div>
+        )
+    }
+}
+export default Header
